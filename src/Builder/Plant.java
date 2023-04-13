3c7e7165be2bc58f39a4bd1197097f2a980d9f96
desarrollo.vca <desarrollo.vca@vcaperu.com>
2023-04-12 20:27:35 -0500

    completed

--- src/Builder/Plant.java
+++ src/Builder/Plant.java
@@ -1,5 +1,37 @@
 package Builder;
 
 public class Plant {
+	private int id;
+	private int age;
+	private int size;
+	private String color;
+	private String scientificName;
+	private String climate;
+	private String lightPreference;
+	public Plant() {}
+	
+	
+
+	public void setId(int id) {
+		this.id=id;
+	}
+	public void setAge(int age) {
+		this.age=age;
+	}
+	public void setSize(int size) {
+		this.size=size;
+	}
+	public void setColor(String color) {
+		this.color=color;
+	}
+	public void setScientificName(String scientificName) {
+		this.scientificName=scientificName;
+	}
+	public void setClimate(String climate) {
+		this.climate=climate;
+	}
+	public void setLightPreference(String lightPreference) {
+		this.lightPreference=lightPreference;
+	}
 
 }
--- src/Builder/PlantBuilder.java
+++ src/Builder/PlantBuilder.java
@@ -1,5 +1,53 @@
 package Builder;
 
 public class PlantBuilder {
+	private int id;
+	private int age;
+	private int size;
+	private String color;
+	private String scientificName;
+	private String climate;
+	private String lightPreference;
+
+	public PlantBuilder setId(int id) {
+		this.id=id;
+		return this;
+	}
+	public PlantBuilder setAge(int age) {
+		this.age=age;
+		return this;
+	}
+	public PlantBuilder setSize(int size) {
+		this.size=size;
+		return this;
+	}
+	public PlantBuilder setColor(String color) {
+		this.color=color;
+		return this;
+	}
+	public PlantBuilder setScientificName(String scientificName) {
+		this.scientificName=scientificName;
+		return this;
+	}
+	public PlantBuilder setClimate(String climate) {
+		this.climate=climate;
+		return this;
+	}
+	public PlantBuilder setLightPreference(String lightPreference) {
+		this.lightPreference=lightPreference;
+		return this;
+	}
+	
+	public Plant build() {
+		Plant plant = new Plant();
+		plant.setId(this.id);
+		plant.setAge(this.age);
+		plant.setSize(this.size);
+		plant.setColor(this.color);
+		plant.setScientificName(this.scientificName);
+		plant.setClimate(this.climate);
+		plant.setLightPreference(this.lightPreference);
+		return plant;
+	}
 
 }
